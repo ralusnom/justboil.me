17b68fbfc37f011dbf04d18e6081e7cf06a027ee
Sam <samgavinio@easyshop.ph>
2015-03-01 14:36:15 +0800

    Set s3 enable to false by default

--- ci/application/config/aws.php
+++ ci/application/config/aws.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['s3']['enable'] = true;
+$config['s3']['enable'] = false;
 $config['s3']['url'] = "http://your-s3-url";
 $config['s3']['key'] = 'SAMPLEKEY';
 $config['s3']['secret'] = 'samplesecret';
